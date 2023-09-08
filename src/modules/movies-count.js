64c04e698173f4db2e7993a31b697ac39e24a93b
Blessing <blessingmike334@gmail.com>
2023-09-08 13:07:14 +0100

    Fix linters error

--- src/modules/movies-count.js
+++ src/modules/movies-count.js
@@ -4,7 +4,7 @@ const movieCount = document.querySelector('.movie-count');
 
 const count = async () => {
   try {
-    const response = await axios.get('https://api.tvmaze.com/shows/');
+    const response = await axios.get('https://api.tvmaze.com/shows/1/episodes');
     const result = response.data;
     const movies = result.splice(0, 12);
     movieCount.innerHTML = movies.length.toString();
